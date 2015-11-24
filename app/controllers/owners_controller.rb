abf36e74f408a253a85079baf99dcde4f6696457
Ricky Hougland <houglande@gmail.com>
2015-11-24 16:12:19 -0600

    switch root view to owners controller, add index and show views for owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,9 @@
 class OwnersController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  root 'owners#index'
+
+  resources :owners do
+    resources :cats
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
