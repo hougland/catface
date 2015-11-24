1c61c3c0969ea697494d866c9d2aa7931db3e871
Ricky Hougland <houglande@gmail.com>
2015-11-24 17:44:25 -0600

    halfway through adding ability to add new cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,17 @@
 class CatsController < ApplicationController
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+    redirect_to 
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :color, :tail_length, :personality,
+      :outdoor, :occupation, :owner_id)
+  end
 end
