4be8c7975f718969441acaf1970d66967dc2d980
Ricky Hougland <houglande@gmail.com>
2015-11-24 16:22:53 -0600

    add ability to create owners

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,6 +4,21 @@ class OwnersController < ApplicationController
   end
 
   def show
+    @owner = Owner.find(params[:id])
+  end
 
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    owner = Owner.create(owner_params)
+    redirect_to owner_path(owner.id)
+  end
+
+  private
+
+  def owner_params
+    params.require(:owner).permit(:name, :city)
   end
 end
