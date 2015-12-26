e237a4d6d58c5ec2ed124786869652e60fea1379
Ricky Hougland <houglande@gmail.com>
2015-12-26 12:20:10 -0600

    test for post create in cats controller

--- spec/controllers/cats_controller_spec.rb
+++ spec/controllers/cats_controller_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe CatsController, type: :controller do
       expect(subject).to render_template :new
     end
   end
+
+  describe "POST 'create'" do
+    it "given good parameters, successfully creates new cat" do
+      expect(Cat.all.length).to eq 0
+      post :create, { cat: { name: "name", owner_id: 1 }, owner_id: 1 }
+      expect(Cat.all.length).to eq 1
+    end
+    it "given bad parameters, does not create new cat" do
+      expect(Cat.all.length).to eq 0
+      post :create, { cat: { name: nil, owner_id: nil }, owner_id: 1 }
+      expect(Cat.all.length).to eq 0
+    end
+  end
 end
