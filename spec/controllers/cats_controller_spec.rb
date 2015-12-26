316d46df09a2dd73ebd9cddcfc3817ef30829557
Ricky Hougland <houglande@gmail.com>
2015-12-26 12:15:49 -0600

    add test for get show in cats controller

--- spec/controllers/cats_controller_spec.rb
+++ spec/controllers/cats_controller_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe CatsController, type: :controller do
   describe "GET 'show'" do
-
+    it "successful show renders show page" do
+      cat = Cat.create(name: "name", owner_id: 1)
+      expect(Cat.all.length).to eq 1
+      get :show, owner_id: cat.owner_id, id: cat.id
+      expect(response.status).to eq 200
+      expect(subject).to render_template :show
+    end
   end
 end
