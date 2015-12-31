cc387e2c7fda65e570275616e9ef29813abfbd53
Ricky Hougland <houglande@gmail.com>
2015-12-30 22:58:17 -0500

    add test for show statuses controller

--- spec/controllers/statuses_controller_spec.rb
+++ spec/controllers/statuses_controller_spec.rb
@@ -8,11 +8,26 @@ RSpec.describe StatusesController, type: :controller do
   let(:cat) do
     Cat.create(name: "fluffy", owner_id: 1)
   end
+
+  let(:status) do
+    Status.create(status: "something", cat_id: cat.id)
+  end
+
   describe "GET 'index'" do
     it "successful index renders index page" do
       get :index, owner_id: owner.id, cat_id: cat.id
       expect(response.status).to eq 200
       expect(subject).to render_template :index
+    end
+  end
+
+  describe "GET 'show'" do
+    it "successful show renders show page" do
+      status
+      expect(Status.all.length).to eq 1
+      get :show, owner_id: owner.id, cat_id: cat.id, id: status.id
+      expect(response.status).to eq 200
+      expect(subject).to render_template :show
     end
   end
 end
